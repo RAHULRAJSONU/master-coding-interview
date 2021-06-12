ebf71715e8f1a9ac2c7131954064cdd9c6207bd6
rahulrajsonu <rajrahul9939@gmail.com>
2021-06-12 10:07:44 +0530

    added test case for the two sum problem

--- src/test/java/io/github/rahulrajsonu/mastercodinginterview/coding/array/TwoSumTest.java
+++ src/test/java/io/github/rahulrajsonu/mastercodinginterview/coding/array/TwoSumTest.java
@@ -1,4 +1,14 @@
+package io.github.rahulrajsonu.mastercodinginterview.coding.array;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class TwoSumTest {
-  
+
+    @Test
+    void selfApproach() {
+        int[] input = new int[]{1,3,7,9,2};
+        assertArrayEquals(new int[]{3,4},TwoSum.selfApproach(input,11));
+    }
 }
