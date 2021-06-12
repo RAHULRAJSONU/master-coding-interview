14c8d53d006050fb6226d7883af3ebb87cd71ede
rahulrajsonu <rajrahul9939@gmail.com>
2021-06-12 10:33:02 +0530

    Twosum solution optimized to handle more scenario

--- src/main/java/io/github/rahulrajsonu/mastercodinginterview/coding/array/TwoSum.java
+++ src/main/java/io/github/rahulrajsonu/mastercodinginterview/coding/array/TwoSum.java
@@ -22,6 +22,6 @@ public class TwoSum {
                 return indices;
             }
         }
-        return indices;
+        throw new RuntimeException("Could not found the numbers");
     }
 }
--- src/test/java/io/github/rahulrajsonu/mastercodinginterview/coding/array/TwoSumTest.java
+++ src/test/java/io/github/rahulrajsonu/mastercodinginterview/coding/array/TwoSumTest.java
@@ -10,5 +10,10 @@ class TwoSumTest {
     void selfApproach() {
         int[] input = new int[]{1,3,7,9,2};
         assertArrayEquals(new int[]{3,4},TwoSum.selfApproach(input,11));
+        assertArrayEquals(new int[]{0,4},TwoSum.selfApproach(input,3));
+        Exception exception = assertThrows(RuntimeException.class, ()-> {
+           TwoSum.selfApproach(input,15);
+        });
+        assertEquals("Could not found the numbers",exception.getMessage());
     }
 }
