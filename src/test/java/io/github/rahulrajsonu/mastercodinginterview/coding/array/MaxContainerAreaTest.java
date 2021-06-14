ddf16af5f548f5ec0acc13852763be71d94fbdd3
rahulrajsonu <rajrahul9939@gmail.com>
2021-06-14 21:13:01 +0530

    Test case added for max container area optimized solution

--- src/test/java/io/github/rahulrajsonu/mastercodinginterview/coding/array/MaxContainerAreaTest.java
+++ src/test/java/io/github/rahulrajsonu/mastercodinginterview/coding/array/MaxContainerAreaTest.java
@@ -10,7 +10,15 @@ class MaxContainerAreaTest {
     void bruitForceSolution() {
         int[] input = new int[]{1,3,4,5,4,3};
         int[] input1 = new int[]{7,1,2,3,9};
+        int[] input2 = new int[]{1};
+        int[] input3 = new int[0];
         assertEquals(MaxContainerArea.bruitForceSolution(input),12);
+        assertEquals(MaxContainerArea.optimizedSolution(input),12);
         assertEquals(MaxContainerArea.bruitForceSolution(input1),28);
+        assertEquals(MaxContainerArea.optimizedSolution(input1),28);
+        assertEquals(MaxContainerArea.bruitForceSolution(input2),0);
+        assertEquals(MaxContainerArea.optimizedSolution(input2),0);
+        assertEquals(MaxContainerArea.bruitForceSolution(input3),0);
+        assertEquals(MaxContainerArea.optimizedSolution(input3),0);
     }
 }
