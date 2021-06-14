301c15f17537df208ffeecd6d229a6ba03e6f8b0
rahulrajsonu <rajrahul9939@gmail.com>
2021-06-14 08:54:06 +0530

    Another test scenario added for max container area problem

--- src/test/java/io/github/rahulrajsonu/mastercodinginterview/coding/array/MaxContainerAreaTest.java
+++ src/test/java/io/github/rahulrajsonu/mastercodinginterview/coding/array/MaxContainerAreaTest.java
@@ -9,6 +9,8 @@ class MaxContainerAreaTest {
     @Test
     void bruitForceSolution() {
         int[] input = new int[]{1,3,4,5,4,3};
+        int[] input1 = new int[]{7,1,2,3,9};
         assertEquals(MaxContainerArea.bruitForceSolution(input),12);
+        assertEquals(MaxContainerArea.bruitForceSolution(input1),28);
     }
 }
