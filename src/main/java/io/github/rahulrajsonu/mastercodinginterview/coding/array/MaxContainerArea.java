502ae30c88fa20a97c1f2b60cb05f4d547d6a2ee
rahulrajsonu <rajrahul9939@gmail.com>
2021-06-14 08:22:55 +0530

    Max container area problem added

---
+++ src/main/java/io/github/rahulrajsonu/mastercodinginterview/coding/array/MaxContainerArea.java
@@ -0,0 +1,22 @@
+package io.github.rahulrajsonu.mastercodinginterview.coding.array;
+
+/**
+ * Problem Statement: You are given an array of positive integers where each integers represents the height of a vertical line on a chart.
+ * Find two lines which together with the X-axis forms a container that would hold the greatest amount of water.
+ * Return the area of water, it would hold.
+ * Given array [1,3,4,5,4,3]
+ * max area = 16
+ */
+public class MaxContainerArea {
+
+    public static int bruitForceSolution(int[] arr){
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                int area = (Integer.min(arr[i],arr[j])*(j-i));
+                if(area > max) max = area;
+            }
+        }
+        return max;
+    }
+}
