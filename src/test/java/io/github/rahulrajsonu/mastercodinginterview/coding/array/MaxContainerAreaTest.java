f04d8f3e5b42f56b25eca5aab26530948b606bfb
rahulrajsonu <rajrahul9939@gmail.com>
2021-06-14 08:24:35 +0530

    Test case for max container area problem  added

--- src/test/java/io/github/rahulrajsonu/mastercodinginterview/coding/array/MaxContainerAreaTest.java
+++ src/test/java/io/github/rahulrajsonu/mastercodinginterview/coding/array/MaxContainerAreaTest.java
@@ -1,4 +1,14 @@
+package io.github.rahulrajsonu.mastercodinginterview.coding.array;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MaxContainerAreaTest {
-  
+
+    @Test
+    void bruitForceSolution() {
+        int[] input = new int[]{1,3,4,5,4,3};
+        assertEquals(MaxContainerArea.bruitForceSolution(input),12);
+    }
 }
