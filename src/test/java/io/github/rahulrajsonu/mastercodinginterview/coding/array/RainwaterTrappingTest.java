05cf57455fb06d2b5f27092b84dbdbe9d77b06f9
rahulrajsonu <rajrahul9939@gmail.com>
2021-06-15 21:54:56 +0530

    test cases added for Rainwater trapping problem

--- src/test/java/io/github/rahulrajsonu/mastercodinginterview/coding/array/RainwaterTrappingTest.java
+++ src/test/java/io/github/rahulrajsonu/mastercodinginterview/coding/array/RainwaterTrappingTest.java
@@ -1,4 +1,20 @@
+package io.github.rahulrajsonu.mastercodinginterview.coding.array;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class RainwaterTrappingTest {
-  
+
+    @Test
+    void bruteforceSolution() {
+        int[] input = new int[]{0,1,0,2,1,0,3,1,0,1,2};
+        int[] input1 = new int[0];
+        int[] input2 = new int[]{3};
+        int[] input3 = new int[]{3,4,3};
+        assertEquals(8,RainwaterTrapping.bruitforceSolution(input));
+        assertEquals(0,RainwaterTrapping.bruitforceSolution(input1));
+        assertEquals(0,RainwaterTrapping.bruitforceSolution(input2));
+        assertEquals(0,RainwaterTrapping.bruitforceSolution(input3));
+    }
 }
