89d4b68e71563892036d88dada53dd07d1fbd6d5
rahulrajsonu <rajrahul9939@gmail.com>
2021-06-15 21:54:13 +0530

    Rainwater Trapping problem added

--- src/main/java/io/github/rahulrajsonu/mastercodinginterview/coding/array/RainwaterTrapping.java
+++ src/main/java/io/github/rahulrajsonu/mastercodinginterview/coding/array/RainwaterTrapping.java
@@ -1,2 +1,15 @@
-package io.github.rahulrajsonu.mastercodinginterview.coding.array;public class RainwaterTrapping {
+package io.github.rahulrajsonu.mastercodinginterview.coding.array;
+
+/**
+ * Problem Statement: Given an array of integer representing an elevation map where the width of each bar is 1.
+ * Return how much rainwater can be trapped.
+ * Given: [0,1,0,2,1,0,3,1,0,1,2]
+ * returns 8
+ */
+public class RainwaterTrapping {
+
+    public static int bruitforceSolution(int[] arr){
+
+    }
+
 }
