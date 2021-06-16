8239858b533c6e1e5c532d59fb84119de9e73c1b
rahulrajsonu <rajrahul9939@gmail.com>
2021-06-16 18:02:13 +0530

    Test case added for rainwater optiomized problem

--- src/test/java/io/github/rahulrajsonu/mastercodinginterview/coding/array/RainwaterTrappingTest.java
+++ src/test/java/io/github/rahulrajsonu/mastercodinginterview/coding/array/RainwaterTrappingTest.java
@@ -17,4 +17,16 @@ class RainwaterTrappingTest {
         assertEquals(0,RainwaterTrapping.bruteforceSolution(input2));
         assertEquals(0,RainwaterTrapping.bruteforceSolution(input3));
     }
+
+    @Test
+    void optimizedSolution() {
+        int[] input = new int[]{0,1,0,2,1,0,3,1,0,1,2};
+        int[] input1 = new int[0];
+        int[] input2 = new int[]{3};
+        int[] input3 = new int[]{3,4,3};
+        assertEquals(8,RainwaterTrapping.optimizedSolution(input));
+        assertEquals(0,RainwaterTrapping.optimizedSolution(input1));
+        assertEquals(0,RainwaterTrapping.optimizedSolution(input2));
+        assertEquals(0,RainwaterTrapping.optimizedSolution(input3));
+    }
 }
