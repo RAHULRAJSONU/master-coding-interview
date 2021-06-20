7555c6fee8ab2c0328a29b481b4ddead95aa7e21
rahulrajsonu <rajrahul9939@gmail.com>
2021-06-20 17:42:01 +0530

    Equal String Check problem added

---
+++ src/main/java/io/github/rahulrajsonu/mastercodinginterview/coding/string/EqualStringCheck.java
@@ -0,0 +1,12 @@
+package io.github.rahulrajsonu.mastercodinginterview.coding.string;
+
+/**
+ * Given two strings S and T,
+ * Return if they equal when both are typed out.
+ * Any'#' that appears in the string counts as a backspace.
+ * Given Input: S= ab#d, T= del##  => not equal
+ * Given Input: S= ab#l, T= at#l  => equal
+ */
+public class EqualStringCheck {
+
+}
