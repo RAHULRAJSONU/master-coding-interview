fb132fdd2307372b4783ba09722451e159c53a6d
rahulrajsonu <rajrahul9939@gmail.com>
2021-06-22 09:56:40 +0530

    Solution added for String equality check

--- src/main/java/io/github/rahulrajsonu/mastercodinginterview/coding/string/EqualStringCheck.java
+++ src/main/java/io/github/rahulrajsonu/mastercodinginterview/coding/string/EqualStringCheck.java
@@ -9,8 +9,29 @@ package io.github.rahulrajsonu.mastercodinginterview.coding.string;
  */
 public class EqualStringCheck {
 
+    /**
+     * Input: ab#c#de##
+     */
     public static boolean check(String s, String t){
-        return Boolean.TRUE;
+        String dS = decode(s);
+        String dT = decode(t);
+        return dS.equals(dT);
+    }
+
+    private static String decode(String s) {
+        String collector = "";
+        int next = 0;
+        while (next < s.length()){
+            if(s.charAt(next)=='#'){
+                if(collector.length()>1) {
+                    collector = collector.substring(0, collector.length() - 2);
+                }
+            } else {
+                collector = collector.concat(String.valueOf(s.charAt(next)));
+            }
+            next++;
+        }
+        return collector;
     }
 
 }
