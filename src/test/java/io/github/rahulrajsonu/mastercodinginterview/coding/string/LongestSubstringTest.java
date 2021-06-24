1d3b83179fae7e1c796bf1097bc8df74465453a6
rahulrajsonu <rajrahul9939@gmail.com>
2021-06-24 10:17:28 +0530

    Test case added for longest substring problem

--- src/test/java/io/github/rahulrajsonu/mastercodinginterview/coding/string/LongestSubstringTest.java
+++ src/test/java/io/github/rahulrajsonu/mastercodinginterview/coding/string/LongestSubstringTest.java
@@ -1,4 +1,13 @@
+package io.github.rahulrajsonu.mastercodinginterview.coding.string;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class LongestSubstringTest {
-  
+
+    @Test
+    void find() {
+        assertEquals(3, LongestSubstring.find("abccabb"));
+    }
 }
