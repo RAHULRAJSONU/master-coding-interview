8a797b7717329002cd856a1ce96605f0f98585e2
rahulrajsonu <rajrahul9939@gmail.com>
2021-06-24 10:16:45 +0530

    Longest substring without duplicate character problem added

---
+++ src/main/java/io/github/rahulrajsonu/mastercodinginterview/coding/string/LongestSubstring.java
@@ -0,0 +1,15 @@
+package io.github.rahulrajsonu.mastercodinginterview.coding.string;
+
+/**
+ * Given a string, find the length of the longest substring without repeating characters
+ * eg; input abccabb => abc, cab => length = 3
+ */
+public class LongestSubstring {
+
+    public static int find(String str) {
+        int length = 0;
+
+        return length;
+    }
+
+}
