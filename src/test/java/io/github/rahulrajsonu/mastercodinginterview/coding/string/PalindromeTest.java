452fb0bd9ff8fc4e8ff6db86e2ce86447516ae2f
rahulrajsonu <rajrahul9939@gmail.com>
2021-06-25 21:04:05 +0530

    Test case added for palindrome

--- src/test/java/io/github/rahulrajsonu/mastercodinginterview/coding/string/PalindromeTest.java
+++ src/test/java/io/github/rahulrajsonu/mastercodinginterview/coding/string/PalindromeTest.java
@@ -1,4 +1,18 @@
+package io.github.rahulrajsonu.mastercodinginterview.coding.string;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PalindromeTest {
-  
+
+    @Test
+    void traverseInwardCheck() {
+        assertTrue(Palindrome.traverseInwardCheck("abcba"));
+        assertTrue(Palindrome.traverseInwardCheck("a;b-Cba"));
+        assertTrue(Palindrome.traverseInwardCheck("a0;b-Cb0a"));
+        assertTrue(Palindrome.traverseInwardCheck("aaa"));
+        assertTrue(Palindrome.traverseInwardCheck("a"));
+        assertTrue(Palindrome.traverseInwardCheck(""));
+    }
 }
