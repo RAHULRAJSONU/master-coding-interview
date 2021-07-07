875c82f840fba48e325bbf47de35d1d32d596435
rahulrajsonu <rajrahul9939@gmail.com>
2021-07-07 12:32:42 +0530

    Utility class for singly linkedlist added

--- src/main/java/io/github/rahulrajsonu/mastercodinginterview/coding/linkedlist/singly/Utils.java
+++ src/main/java/io/github/rahulrajsonu/mastercodinginterview/coding/linkedlist/singly/Utils.java
@@ -1,2 +1,30 @@
-package io.github.rahulrajsonu.mastercodinginterview.coding.linkedlist.singly;public class Utils {
+package io.github.rahulrajsonu.mastercodinginterview.coding.linkedlist.singly;
+
+public class Utils {
+
+    public static Node construct(int[] vals) {
+        Node head = new Node(-1);
+        Node temp = head;
+        for (int val : vals) {
+            head.next = new Node(val);
+            head = head.next;
+        }
+        return temp.next;
+    }
+
+    public static void print (Node head) {
+        if(null==head)return;
+        System.out.print(head.val);
+        if(null != head.next)System.out.print("->");
+        print(head.next);
+    }
+
+    public static boolean match (Node n1, Node n2) {
+        if(null == n1 && null == n2) return Boolean.TRUE;
+        if(n1 != null && n2 != null){
+            System.out.println(n1.val+" -> "+n2.val);
+            return (n1.val==n2.val) && match(n1.next,n2.next);
+        }
+        return Boolean.FALSE;
+    }
 }
