4b70685734b1df845a0395241a24e77c0899c752
rahulrajsonu <rajrahul9939@gmail.com>
2021-07-08 10:41:26 +0530

    Test case added for Reverse liked list

--- src/test/java/io/github/rahulrajsonu/mastercodinginterview/coding/linkedlist/singly/ReverseALinkedListTest.java
+++ src/test/java/io/github/rahulrajsonu/mastercodinginterview/coding/linkedlist/singly/ReverseALinkedListTest.java
@@ -1,4 +1,15 @@
+package io.github.rahulrajsonu.mastercodinginterview.coding.linkedlist.singly;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ReverseALinkedListTest {
-  
+
+    @Test
+    void reverse() {
+        int[] vals = new int[]{1,2,3,4,5,6};
+        int[] rev = new int[]{6,5,4,3,2,1};
+        assertTrue(Utils.match(Utils.construct(rev), ReverseALinkedList.reverse(Utils.construct(vals))));
+    }
 }
