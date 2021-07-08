bc557235872576fb1db62da64196994d3fe590e1
rahulrajsonu <rajrahul9939@gmail.com>
2021-07-08 10:40:46 +0530

    Reverse singly linked list problem added

--- src/main/java/io/github/rahulrajsonu/mastercodinginterview/coding/linkedlist/singly/ReverseALinkedList.java
+++ src/main/java/io/github/rahulrajsonu/mastercodinginterview/coding/linkedlist/singly/ReverseALinkedList.java
@@ -1,2 +1,21 @@
-package io.github.rahulrajsonu.mastercodinginterview.coding.linkedlist.singly;public class ReverseALinkedList {
+package io.github.rahulrajsonu.mastercodinginterview.coding.linkedlist.singly;
+
+/**
+ * Given a singly linked list, reverse it
+ * eg; input = 1->2->3->4->5->6 | output = 6->5->4->3->2->1
+ */
+public class ReverseALinkedList {
+
+    public static Node reverse(Node head) {
+        if(null==head || null==head.next)return head;
+        Node curr = head;
+        Node prev = null;
+        while (null != curr) {
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
 }
