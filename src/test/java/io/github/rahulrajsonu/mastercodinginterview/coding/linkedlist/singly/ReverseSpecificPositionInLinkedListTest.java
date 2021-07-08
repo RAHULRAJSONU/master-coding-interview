09ac1243459dba0ce9f60046fae7d4dd6ed65bfd
rahulrajsonu <rajrahul9939@gmail.com>
2021-07-08 19:11:03 +0530

    Test case for Reverse between in linkedlist problem added

--- src/test/java/io/github/rahulrajsonu/mastercodinginterview/coding/linkedlist/singly/ReverseSpecificPositionInLinkedListTest.java
+++ src/test/java/io/github/rahulrajsonu/mastercodinginterview/coding/linkedlist/singly/ReverseSpecificPositionInLinkedListTest.java
@@ -1,4 +1,14 @@
+package io.github.rahulrajsonu.mastercodinginterview.coding.linkedlist.singly;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ReverseSpecificPositionInLinkedListTest {
-  
+
+    @Test
+    void reverse() {
+        int[] vals = new int[]{1,2,3,4,5,6};
+        Utils.print(ReverseSpecificPositionInLinkedList.reverse(Utils.construct(vals),2,4));
+    }
 }
